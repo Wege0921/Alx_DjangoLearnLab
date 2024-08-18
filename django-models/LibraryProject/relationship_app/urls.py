33be34af7f770b164434d65af31ba532d21e8b9a
Wege0921 <wegemuluneh@gmail.com>
2024-08-18 12:07:45 -0700

    relationship_app/

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,3 +5,13 @@ urlpatterns = [
     # other paths...
 ]
 
+
+# relationship_app/urls.py
+from django.urls import path
+from .views import LibraryDetailView
+
+urlpatterns = [
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    # Other URL patterns
+]
+
