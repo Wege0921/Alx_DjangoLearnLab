9ecfb921ea930f8b38c9d4f8c8345d0de2fc480b
Wege0921 <wegemuluneh@gmail.com>
2024-08-18 13:25:43 -0700

    url corrected

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,6 +8,9 @@ from .views import LibraryDetailView
 urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('relationship_app/', include('relationship_app.urls')),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
     # Other URL patterns
 ]
 
