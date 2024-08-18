d35af7ba7e1c8a37b3def91108da35253818da3b
Wege0921 <wegemuluneh@gmail.com>
2024-08-18 00:48:29 -0700

    url config

---
+++ django-models/relationship_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('relationship_app/', include('relationship_app.urls')),
+]
+
