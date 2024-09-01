303bec1580ddcc2cc0be49681cfc47b0679fabb8
Wege0921 <wegemuluneh@gmail.com>
2024-09-01 02:51:25 -0700

    crud

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,3 +7,7 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
