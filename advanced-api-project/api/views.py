4d1e76e1573c259ed5b06e30a8d20a06ae526f3e
Wege0921 <wegemuluneh@gmail.com>
2024-09-08 06:39:01 -0700

    filters.OrderingFilter

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ from .models import Book
 from .serializers import BookSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
-
+from rest_framework.filters import OrderingFilter
 from django_filters import rest_framework
 from rest_framework.filters import SearchFilter, OrderingFilter  # Import OrderingFilter
 
