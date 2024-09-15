471d341309bab0ff0aa4b072e2fc26322f424905
Wege0921 <wegemuluneh@gmail.com>
2024-09-15 13:05:25 -0700

    implementing the blog user-auth

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# blog/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django import forms
+
+# Custom Registration Form extending UserCreationForm
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+# Registration View
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after registration
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile View
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        user = request.user
+        user.email = request.POST.get('email')
+        user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html')
+
