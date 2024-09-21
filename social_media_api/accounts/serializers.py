ada32ab4392f6f48a7fb994928e58e81d7d9afff
Wege0921 <wegemuluneh@gmail.com>
2024-09-21 13:15:07 -0700

    serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,22 +3,40 @@ from .models import CustomUser
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+
+# Getting the custom user model
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'password', 'email']
+    # Adding fields for password validation
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True, required=True)
 
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password_confirm', 'bio', 'profile_picture')
+
+    # Custom validation for password match
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    # Overriding the create method to handle password hashing
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # Remove password_confirm from validated_data as it's not part of the User model
+        validated_data.pop('password_confirm')
+
+        # Creating the user using the create_user method
+        user = User.objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
-            email=validated_data['email']
+            bio=validated_data.get('bio', ''),  # Optional field
+            profile_picture=validated_data.get('profile_picture', None)  # Optional field
         )
-        Token.objects.create(user=user)  # Create a token for the new user
+
         return user
 
